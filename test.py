6f92a60535de971619ee7bf0b6a69549bcc6b93e
sscott-tantalumlabs <shane.scott@tantalumlabs.io>
2025-08-11 16:30:48 -0500

    Remove tests

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-AWS_ACCESS_KEY_ID="AKIAEXAMPLEKEY12345678"
-AWS_SECRET_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-
-user_input = input("Enter command: ")
-os.system("echo " + user_input)  # 🚨 CodeQL will flag this as command injection
-
